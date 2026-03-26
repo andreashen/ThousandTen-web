e92ca170406a6bac2abdd8fd019823ad63fb88d3
andreashen <16459552+andreashen@users.noreply.github.com>
2026-03-27 00:08:40 +0800

    chore: add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ThousandTen-web/', // 改成你的仓库名
   plugins: [react(), tailwindcss()],
 })
